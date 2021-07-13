05b9d604cd9f9526beb6b1847342ceee64dac685
jherna <jherna76@elpuig.xeill.net>
2021-07-13 23:30:21 +0200

    refactor test, add beforeEach

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -6,24 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * https://osherove.com/tdd-kata-1
  */
 public class StringCalculatorTest {
+    StringCalculator sc;
+
+    @BeforeEach
+    public void createStringCalculator() {
+        sc = new StringCalculator();
+    }
 
     @Test
     public void addVoidStringTest() {
-        StringCalculator sc = new StringCalculator();
         int sum = sc.add("");
         assertEquals(sum,0);
     }
 
     @Test
     public void addRandomSingleNumberStringTest() {
-        StringCalculator sc = new StringCalculator();
         int sum = sc.add("1200");
         assertEquals(sum,1200);
     }
 
     @Test
     public void addNumbersStringTest() {
-        StringCalculator sc = new StringCalculator();
         int sum = sc.add("1\n2,3;4");
         assertEquals(sum,10);
     }
@@ -37,24 +41,18 @@ public class StringCalculatorTest {
 
     @Test
     public void addOneNegativeNumbersStringTest() {
-        StringCalculator sc = new StringCalculator();
-        //int sum = sc.add("-1");
         ArithmeticException e = assertThrows(ArithmeticException.class,() -> sc.add("-2"));
         assertEquals("Negatives not allowed: -2", e.getMessage());
     }
 
     @Test
     public void addOneNegativeAndPositiveNumberStringTest() {
-        StringCalculator sc = new StringCalculator();
-        //int sum = sc.add("-1");
         ArithmeticException e = assertThrows(ArithmeticException.class,() -> sc.add("1,-2"));
         assertEquals("Negatives not allowed: -2", e.getMessage());
     }
 
     @Test
     public void addTwoNegativeAndPositiveNumberStringTest() {
-        StringCalculator sc = new StringCalculator();
-        //int sum = sc.add("-1");
         ArithmeticException e = assertThrows(ArithmeticException.class,() -> sc.add("1,-2,-3"));
         assertEquals("Negatives not allowed: -2,-3", e.getMessage());
     }
